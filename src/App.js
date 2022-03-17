587e57be9b6f1ae48295a17d67eda59ee7c9d105
ryutaro-kimura <sasuker2264@gmail.com>
2022-03-17 21:44:49 +0900

    step2:コンポーネントの作成と利用

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { List } from "./List";
+import { useState } from 'react';
+
 function App() {
+  const [description, setDescription] = useState('クリック前の表示です')
+  
+  const changeDescription = () => {
+      setDescription("クリック後の表示です")
+  }
   return (
-    <div>hogehoge</div>
+    <div>
+      {description}
+      <List title="取扱言語一覧"/>
+      <button onClick={changeDescription}>ボタン</button>
+    </div>
   );
 }
 

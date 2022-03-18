6c5b30d323371c87218cca4f6d3108290da1a28e
ryutaro-kimura <sasuker2264@gmail.com>
2022-03-18 21:23:10 +0900

    step4: 条件分岐とループ

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { List } from "./List";
 import { useState } from 'react';
+import { Form } from "./Form";
 
 function App() {
-  const [description, setDescription] = useState('クリック前の表示です')
-  
+  const [description, setDescription] = useState('クリック前の表示です');
+  const [tab, setTab] = useState('list');
+
   const changeDescription = () => {
       setDescription("クリック後の表示です")
   }
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr />
       {description}
-      <List title="取扱言語一覧"/>
+      {
+        tab === 'list' ? <List title="取扱言語一覧"/> : <Form />
+      }
       <button onClick={changeDescription}>ボタン</button>
     </div>
   );
--- src/List.js
+++ src/List.js
@@ -1,7 +1,21 @@
+const LANGUAGES = [ // 追加
+  'JavaScript',
+  'C++',
+  'Ruby',
+  'Java',
+  'PHP',
+  'Go'
+];
+
 export const List = ({ title }) => {
     return(
         <div>
             <h4>{ title }</h4>
+            {
+                LANGUAGES.map((lang, index) => {
+                    return <div key={index}>{ lang }</div>
+                })
+            }
             <div>リストです</div>
         </div>
     )
